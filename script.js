68550910a25d338be604f24736748fb505320817
Phong Che <pche3@illinois.edu>
2018-02-05 15:37:41 -0600

    Make changes to content

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $(document).ready( function() {
     document.getElementById("proj_link").href ="https://pche3.github.io/quote_machine/";
     document.getElementById("picture").src ="https://github.com/pche3/quote_machine/blob/gh-pages/preview1.png?raw=true";
     document.getElementById("picture").alt ="Quote Machine";
-    $("#context").html("Come checkout this quote generator that utilizes an API to retrieve quotes. Once a JSON object is retrieved, the author and quote can be parsed. The user can tweet the quote through Twitter through a dedicated Twitter button.");
+    $("#context").html("Utilize an API to retrieve quotes. The user can tweet a quote through a dedicated Twitter button.");
     });
   $("#project2").on("click", function() {
     document.getElementById("proj_link").href ="https://pche3.github.io/theremin/";
     document.getElementById("picture").src ="https://github.com/pche3/theremin/blob/master/preview_theremin.png?raw=trues";
     document.getElementById("picture").alt ="Digital Theremin";
-    $("#context").html("This website is dedicated to a project that me and friends worked on. Also, it is the first website I made to pratice front-end development. The project is all about developing a new product that eliminates previous challenges analog theremin faces.");
+    $("#context").html("ECE 445 Senior Project: A modern take on the classic analog theremin");
     });
 })
