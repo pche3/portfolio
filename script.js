18149b434d944ab0857aafcb0d94e8acb69c4290
Phong Che <pche3@illinois.edu>
2018-02-05 14:40:12 -0600

    Brush up on the about part with profile picture and projects

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ $(document).ready( function() {
   $("#project2").on("click", function() {
     document.getElementById("picture").src ="https://github.com/pche3/theremin/blob/master/preview_theremin.png?raw=trues";
     document.getElementById("picture").alt ="Digital Theremin";
-    $("#context").html("This website is dedicated to a project that me and friends worked on. Also, it is the first website I made to pratice front-end development. The project is all about developing a new product that eliminates previous generation design challenges.");
+    $("#context").html("This website is dedicated to a project that me and friends worked on. Also, it is the first website I made to pratice front-end development. The project is all about developing a new product that eliminates previous challenges analog theremin faces.");
     });
 })
